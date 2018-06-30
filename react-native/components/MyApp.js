ace7bd1a28b71009e707043b783ab3ac6add6693
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-06-30 17:15:28 +0200

    fix some react warnings

--- react-native/components/MyApp.js
+++ react-native/components/MyApp.js
@@ -1,143 +1,119 @@
-import React, { Component } from 'react'
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
-import { Callout } from 'react-native-maps';
-import {
-    View,
-    StyleSheet,
-    Button,
-    Alert,
-    TouchableOpacity,
-    Text,
-    TextInput,
-    ScrollView,
-    AsyncStorage,
-    Navigator,
-    StatusBar
- } from 'react-native';
-
+import PropTypes from 'prop-types'
+import React from 'react'
+import MapView, {Marker} from 'react-native-maps'
+import {View, StyleSheet} from 'react-native'
 
 
 const data = {
-
-            region : {
-                latlng: {
-                        latitude: 0,
-                        longitude: 0,
-                        latitudeDelta: 0.015,
-                        longitudeDelta: 0.0121,
-                    },
-                    title : 'title',
-                    description : 'description'
-            },
-
-            me_marker : {
-                latlng: {
-                    latitude: 37.785834,
-                    longitude: -122.406417,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                },
-                title : 'alex',
-                description : 'description'
-            },
-
-            other_marker : [
-            {
-                latlng: {
-                    latitude: 37.786935,
-                    longitude: -122.407416,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                },
-                title : 'nombre d utilisateur : ',
-                description : 3,
-            },
-            {
-                latlng: {
-                    latitude: 37.784935,
-                    longitude: -122.407416,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                },
-                title : 'nombre d utilisateur : ',
-                description : 3,
-            },
-            {
-                latlng: {
-                    latitude: 37.784935,
-                    longitude: -122.405416,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                },
-                title : 'nombre d utilisateur : ',
-                description : 3,
-            },
-            {
-                latlng: {
-                    latitude: 37.786935,
-                    longitude: -122.405416,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                },
-                title : 'nombre d utilisateur : ',
-                description : 3,
-            },
-            ]
-
-        }
+  region : {
+    latlng: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.015,
+      longitudeDelta: 0.0121,
+    },
+    title : 'title',
+    description : 'description'
+  },
+  me_marker : {
+    latlng: {
+      latitude: 37.785834,
+      longitude: -122.406417,
+      latitudeDelta: 0.015,
+      longitudeDelta: 0.0121,
+    },
+    title : 'alex',
+    description : 'description'
+  },
+  other_marker : [
+    {
+      latlng: {
+        latitude: 37.786935,
+        longitude: -122.407416,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      },
+      title : 'nombre d utilisateur : ',
+      description : '3',
+    },
+    {
+      latlng: {
+        latitude: 36.784935,
+        longitude: -122.407416,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      },
+      title : 'nombre d utilisateur : ',
+      description : '3',
+    },
+    {
+      latlng: {
+        latitude: 37.784935,
+        longitude: -122.405416,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      },
+      title : 'nombre d utilisateur : ',
+      description : '3',
+    },
+    {
+      latlng: {
+        latitude: 38.786935,
+        longitude: -122.405416,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      },
+      title : 'nombre d utilisateur : ',
+      description : '3',
+    },
+  ]
+}
 
 
 export default class MyApp extends React.Component {
-
-
-
-
-    render() {
-
-    const { region, latitude, longitude } = this.props;
-    console.log(this.props);
-
-
-
-    var lat = this.props.latitude ? this.props.latitude : 0
-    var longit = this.props.longitude ? this.props.longitude : 0
-    console.log(lat, longit)
-
-
+  static propTypes = {
+    latitude:  PropTypes.number,
+    longitude: PropTypes.number,
+  }
+  static defaultProps = {
+    latitude:  0,
+    longitude: 0,
+  }
+  render() {
+    const { latitude, longitude } = this.props
     return (
       <View style ={styles.container}>
-            <MapView
-              style={styles.map}
-              region={{
-                latitude: lat,
-                longitude: longit,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-              }}
-            >
-                <Marker draggable
-                    coordinate={data.me_marker.latlng}
-                    onDragEnd={(e) => this.setState({ x : e.nativeEvent.coordinate })}
-                />
-                <Marker
-                    coordinate={data.me_marker.latlng}
-                    title={data.me_marker.title}
-                    description={data.me_marker.description}
-                />
-                {data.other_marker.map(marker => (
-                    <Marker
-                      coordinate={marker.latlng}
-                      title={marker.title}
-                      description={marker.description}
-                    />
-                ))}
-            </MapView>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+          <Marker draggable
+            coordinate={data.me_marker.latlng}
+            onDragEnd={(e) => this.setState({ x : e.nativeEvent.coordinate })}
+          />
+          <Marker
+            coordinate={data.me_marker.latlng}
+            title={data.me_marker.title}
+            description={data.me_marker.description}
+          />
+          {data.other_marker.map(marker => (
+            <Marker
+              key={JSON.stringify(marker.latlng)}
+              coordinate={marker.latlng}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
       </View>
-    );
+    )
   }
 }
-
 
 
 const styles = StyleSheet.create({
@@ -154,4 +130,4 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-});
+})
